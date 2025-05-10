2bd6a5a70725616e0f5ddd9616ced2e43151b505
YunussEmree <senyigityunusemre@gmail.com>
2025-05-10 04:44:14 +0300

    Adds initial WinForms app structure
    Sets up a basic Windows Forms application with a data grid and CRUD operations.
    
    Initializes the UI with text boxes and buttons.
    Implements database connectivity and data display.
    Includes basic CRUD functionality.
    Adds a .gitignore file to exclude build artifacts and user-specific files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblTelefon = new System.Windows.Forms.Label();
+            this.txtSoyisim = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtİsim = new System.Windows.Forms.TextBox();
+            this.lblSoyisim = new System.Windows.Forms.Label();
+            this.lblİsim = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 8);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(582, 144);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(188, 36);
+            this.btnEkle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(70, 19);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(188, 68);
+            this.btnGuncelle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(70, 19);
+            this.btnGuncelle.TabIndex = 2;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(38, 183);
+            this.btnListele.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(70, 19);
+            this.btnListele.TabIndex = 3;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(38, 223);
+            this.btnSil.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(70, 19);
+            this.btnSil.TabIndex = 4;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtTelefon);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.lblTelefon);
+            this.groupBox1.Controls.Add(this.txtSoyisim);
+            this.groupBox1.Controls.Add(this.btnGuncelle);
+            this.groupBox1.Controls.Add(this.lblEmail);
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.txtİsim);
+            this.groupBox1.Controls.Add(this.lblSoyisim);
+            this.groupBox1.Controls.Add(this.lblİsim);
+            this.groupBox1.Location = new System.Drawing.Point(315, 156);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(276, 129);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(68, 88);
+            this.txtTelefon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(95, 20);
+            this.txtTelefon.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(68, 68);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(95, 20);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // lblTelefon
+            // 
+            this.lblTelefon.AutoSize = true;
+            this.lblTelefon.Location = new System.Drawing.Point(26, 90);
+            this.lblTelefon.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTelefon.Name = "lblTelefon";
+            this.lblTelefon.Size = new System.Drawing.Size(43, 13);
+            this.lblTelefon.TabIndex = 3;
+            this.lblTelefon.Text = "Telefon";
+            // 
+            // txtSoyisim
+            // 
+            this.txtSoyisim.Location = new System.Drawing.Point(68, 46);
+            this.txtSoyisim.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSoyisim.Name = "txtSoyisim";
+            this.txtSoyisim.Size = new System.Drawing.Size(95, 20);
+            this.txtSoyisim.TabIndex = 8;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(26, 68);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtİsim
+            // 
+            this.txtİsim.Location = new System.Drawing.Point(68, 25);
+            this.txtİsim.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtİsim.Name = "txtİsim";
+            this.txtİsim.Size = new System.Drawing.Size(95, 20);
+            this.txtİsim.TabIndex = 7;
+            // 
+            // lblSoyisim
+            // 
+            this.lblSoyisim.AutoSize = true;
+            this.lblSoyisim.Location = new System.Drawing.Point(26, 49);
+            this.lblSoyisim.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSoyisim.Name = "lblSoyisim";
+            this.lblSoyisim.Size = new System.Drawing.Size(42, 13);
+            this.lblSoyisim.TabIndex = 1;
+            this.lblSoyisim.Text = "Soyisim";
+            // 
+            // lblİsim
+            // 
+            this.lblİsim.AutoSize = true;
+            this.lblİsim.Location = new System.Drawing.Point(26, 27);
+            this.lblİsim.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblİsim.Name = "lblİsim";
+            this.lblİsim.Size = new System.Drawing.Size(25, 13);
+            this.lblİsim.TabIndex = 0;
+            this.lblİsim.Text = "İsim";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 294);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 292);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button btnEkle;
+        private Button btnGuncelle;
+        private Button btnListele;
+        private Button btnSil;
+        private GroupBox groupBox1;
+        private TextBox txtTelefon;
+        private Label lblTelefon;
+        private Label lblEmail;
+        private Label lblSoyisim;
+        private Label lblİsim;
+        private Label label4;
+        private TextBox txtEmail;
+        private TextBox txtSoyisim;
+        private TextBox txtİsim;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Data Source=YunusEmresPC;Initial Catalog=windowsformapp;Integrated Security=True";
+        private int selectedId = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupDataGridView();
+            CreateDatabaseIfNotExists();
+            btnEkle.Click += BtnEkle_Click;
+            btnGuncelle.Click += BtnGuncelle_Click;
+            btnListele.Click += BtnListele_Click;
+            btnSil.Click += BtnSil_Click;
+            dataGridView1.SelectionChanged += DataGridView1_SelectionChanged;
+            RefreshDataGridView();
+        }
+
+        private void CreateDatabaseIfNotExists()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string createTableQuery = @"
+                        IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Contacts')
+                        BEGIN
+                            CREATE TABLE Contacts (
+                                Id INT IDENTITY(1,1) PRIMARY KEY,
+                                İsim NVARCHAR(50),
+                                Soyisim NVARCHAR(50),
+                                Email NVARCHAR(100),
+                                Telefon NVARCHAR(20)
+                            )
+                        END";
+                    using (SqlCommand cmd = new SqlCommand(createTableQuery, conn))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Veritabanı oluşturma hatası: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SetupDataGridView()
+        {
+            dataGridView1.Columns.Add("Id", "Id");
+            dataGridView1.Columns.Add("İsim", "İsim");
+            dataGridView1.Columns.Add("Soyisim", "Soyisim");
+            dataGridView1.Columns.Add("Email", "Email");
+            dataGridView1.Columns.Add("Telefon", "Telefon");
+            dataGridView1.Columns["Id"].Visible = false;
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                try
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        string insertQuery = @"INSERT INTO Contacts (İsim, Soyisim, Email, Telefon) 
+                                             VALUES (@İsim, @Soyisim, @Email, @Telefon)";
+                        using (SqlCommand cmd = new SqlCommand(insertQuery, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@İsim", txtİsim.Text);
+                            cmd.Parameters.AddWithValue("@Soyisim", txtSoyisim.Text);
+                            cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                            cmd.Parameters.AddWithValue("@Telefon", txtTelefon.Text);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    RefreshDataGridView();
+                    ClearInputs();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Kayıt ekleme hatası: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            if (selectedId >= 0 && ValidateInputs())
+            {
+                try
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        string updateQuery = @"UPDATE Contacts 
+                                             SET İsim = @İsim, Soyisim = @Soyisim, 
+                                                 Email = @Email, Telefon = @Telefon 
+                                             WHERE Id = @Id";
+                        using (SqlCommand cmd = new SqlCommand(updateQuery, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@Id", selectedId);
+                            cmd.Parameters.AddWithValue("@İsim", txtİsim.Text);
+                            cmd.Parameters.AddWithValue("@Soyisim", txtSoyisim.Text);
+                            cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                            cmd.Parameters.AddWithValue("@Telefon", txtTelefon.Text);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    RefreshDataGridView();
+                    ClearInputs();
+                    selectedId = -1;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Güncelleme hatası: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen güncellenecek bir kayıt seçin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            RefreshDataGridView();
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            if (selectedId >= 0)
+            {
+                try
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        string deleteQuery = "DELETE FROM Contacts WHERE Id = @Id";
+                        using (SqlCommand cmd = new SqlCommand(deleteQuery, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@Id", selectedId);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    RefreshDataGridView();
+                    ClearInputs();
+                    selectedId = -1;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Silme hatası: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen silinecek bir kayıt seçin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void DataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                selectedId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id"].Value);
+                txtİsim.Text = dataGridView1.SelectedRows[0].Cells["İsim"].Value.ToString();
+                txtSoyisim.Text = dataGridView1.SelectedRows[0].Cells["Soyisim"].Value.ToString();
+                txtEmail.Text = dataGridView1.SelectedRows[0].Cells["Email"].Value.ToString();
+                txtTelefon.Text = dataGridView1.SelectedRows[0].Cells["Telefon"].Value.ToString();
+            }
+        }
+
+        private void RefreshDataGridView()
+        {
+            try
+            {
+                dataGridView1.Rows.Clear();
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string selectQuery = "SELECT * FROM Contacts";
+                    using (SqlCommand cmd = new SqlCommand(selectQuery, conn))
+                    {
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                dataGridView1.Rows.Add(
+                                    reader["Id"],
+                                    reader["İsim"],
+                                    reader["Soyisim"],
+                                    reader["Email"],
+                                    reader["Telefon"]
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Veri listeleme hatası: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ClearInputs()
+        {
+            txtİsim.Clear();
+            txtSoyisim.Clear();
+            txtEmail.Clear();
+            txtTelefon.Clear();
+        }
+
+        private bool ValidateInputs()
+        {
+            if (string.IsNullOrWhiteSpace(txtİsim.Text) ||
+                string.IsNullOrWhiteSpace(txtSoyisim.Text) ||
+                string.IsNullOrWhiteSpace(txtEmail.Text) ||
+                string.IsNullOrWhiteSpace(txtTelefon.Text))
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurun.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+    }
+}
